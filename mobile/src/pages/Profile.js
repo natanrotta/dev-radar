74f011e3663a6a373b0459ca2344f91450e05338
natan_rotta <natanrotta@gmail.com>
2020-01-19 09:46:35 -0200

    Finaliza aplicação Mobile

---
+++ mobile/src/pages/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+function Profile({ navigation }) {
+
+    const githubusername = navigation.getParam('github_username')
+
+    return (
+        <WebView style={{ flex: 1 }} source={{ uri: `https://github.com/${githubusername}` }} />
+    )
+}
+
+export default Profile;
